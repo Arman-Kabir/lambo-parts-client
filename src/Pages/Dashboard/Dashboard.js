9b3619c8159baa7a6300e6e94731ccda5f611d45
Arman-Kabir <armankabir8@gmail.com>
2022-05-24 14:51:15 +0600

    my orders page done

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -17,7 +17,7 @@ const Dashboard = () => {
 
             <div class="drawer-side">
                 <label for="dashboard-sidebar" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
                     <li><Link to="/dashboard">My Orders</Link></li>
                     <li><Link to="/dashboard/addareview">Add a Review</Link></li>
--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -5,6 +5,8 @@ import auth from '../../firebase.init';
 const MyOrders = () => {
     const [myorders, setMyOrders] = useState([]);
     const [user] = useAuthState(auth);
+    // console.log(myorders);
+    // console.log(user);
     useEffect(() => {
         if (user) {
             fetch(`http://localhost:5000/orders?userEmail=${user.email}`)
@@ -16,7 +18,48 @@ const MyOrders = () => {
     }, [user])
     return (
         <div>
-            <h2>My orders...{myorders.length}</h2>
+            {/* Card to show user information */}
+
+            <div class="card w-96 bg-base-100 shadow-xl my-4">
+                <div class="card-body">
+                    <h2 class="font-bold text-xl text-primary"> My orders...{myorders.length}</h2>
+                    <div>
+                        <p>Name: {user.displayName}</p>
+                        <p>Email: {user.email}</p>                       
+                        {/* <span>Name: {user.email}</span> */}
+                    </div>
+                </div>
+            </div>
+
+            {/* Table to show Order Details */}
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Parts Name</th>
+                            <th>Quantity</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                            <th>Payment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myorders.map((o, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{o.order}</td>
+                                <td>{o.orderQuantity}</td>
+                                <td>{o.phone}</td>
+                                <td>{o.address}</td>
+                                <td>{o.payment
+
+                                }</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
