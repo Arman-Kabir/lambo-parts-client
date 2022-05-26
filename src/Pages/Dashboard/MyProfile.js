9b1aa15066bb2440e10fbab98af70a4bea62af69
Arman-Kabir <armankabir8@gmail.com>
2022-05-26 12:34:29 +0600

    profile page and api done

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const MyProfile = () => {
+    const [profile, setProfile] = useState([]);
     const [user] = useAuthState(auth);
+    // console.log(profile);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/profile?userEmail=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setProfile(data)
+                });
+        }
+    }, [user])
 
     const handleAddToProfile = event => {
-        event.preventDefault();
+        // event.preventDefault();
         console.log('abc');
 
         const education = event.target.education.value;
@@ -16,9 +45,9 @@ const MyProfile = () => {
         const linkedin = event.target.linkedin.value;
         // console.log(rating, description);
 
-        const profile={
-            name:user.displayName,
-            email:user.email,
+        const profile = {
+            name: user.displayName,
+            email: user.email,
             education,
             location,
             phone,
@@ -27,34 +56,39 @@ const MyProfile = () => {
         console.log(profile);
 
         // sending review to server
-        fetch('http://localhost:5000/profile',{
-            method:'POST',
+        fetch('http://localhost:5000/profile', {
+            method: 'POST',
             headers: {
-                'content-type':'application/json'
+                'content-type': 'application/json'
             },
-            body:JSON.stringify(profile)
+            body: JSON.stringify(profile)
         })
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.insertedId){
-                toast.success('profile info added');
-            }
-            console.log(data);
-        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    toast.success('profile info added');
+                }
+                console.log(data);
+            })
     }
     return (
         <div>
             {/* Card to show logged-in user information */}
-            <div class="card w-96 bg-base-100 shadow-xl my-4">
-                <div class="card-body">
+            <div class="card w-96 bg-base-100 shadow-xl my-4 py-2">
+                <div class="">
                     <h2 class="font-bold text-xl text-primary"> {user.displayName}'s Profile...</h2>
                     <p>Email: {user.email}</p>
                 </div>
             </div>
+
             {/* my profile information */}
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <div class="card-body">
-                    <h2 class="text-center font-bold text-success">My Profile</h2>
+            <div class="card w-96 bg-base-100 shadow-xl p-2">
+                <div class="">
+                    <h2 class="text-center font-bold text-success">{profile.length > 0 ? <p>My Profile</p> : <p>Profile Data Not Added</p>}</h2>
+                    <p className='text-xl font-bold'>{profile?.education}</p>
+                    <p className='text-xl font-bold'>{profile?.location}</p>
+                    <p className='text-xl font-bold'>{profile?.phone}</p>
+                    <p className='text-xl font-bold'>{profile?.linkedin}</p>
 
                 </div>
             </div>
